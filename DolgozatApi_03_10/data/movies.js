75fc8d790bdb08eb0b72da2e75574bd1f0951c4c
Farkas Benedek <>
2025-03-10 11:05:53 +0100

    alap

---
+++ DolgozatApi_03_10/data/movies.js
@@ -0,0 +1,7 @@
+const movies = [
+    {title: "Oppenheimer", year: 2023, director: "Christopher Nolan", oscaraward: true},
+    {title: "The Dark Knight", year: 2008, director: "Christopher Nolan", oscaraward: false},
+    {title: "Inception", year: 2010, director: "Christopher Nolan", oscaraward: false},
+]
+
+export default movies
